9b11a5c9c0ece6de8e79ade68aed679c5a94a5e2
Karel Blavka <karel.blavka@bigclown.com>
2016-08-16 09:36:41 +0200

    clean python code

--- ft260/tools.py
+++
@@ -1,8 +0,0 @@
-import functools
-
-def human_print_gpio(data:bytes):
-    dir = {0:'INPUT', 1:'OUTPUT'}
-    for i in range(0,5):
-        print( 'GPIO'+str(i), data[0] >> i & 1, dir[data[1] >> i & 1] )
-    for i,p in enumerate(('A','B','C','D','E','F','G', 'H')):
-        print( 'GPIO'+p, data[0] >> i & 1, dir[data[1] >> i & 1] )
