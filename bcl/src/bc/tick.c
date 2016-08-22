c6382b04ae41f6e832ed0d952f4d3895e42577c5
Karel Blavka <karel.blavka@bigclown.com>
2016-08-22 13:30:52 +0200

    reimplementace tick

--- bcl/src/bc/tick.c
+++ bcl/src/bc/tick.c
@@ -4,12 +4,15 @@
 struct timeval start;
 struct timeval now;
 
-void bc_tick_init(){
-	gettimeofday(&start,NULL);
-}
-
 bc_tick_t bc_tick_get(void)
 {
+	
+	if(start.tv_sec==0)
+	{
+		gettimeofday(&start,NULL);
+	}
+	
 	gettimeofday(&now,NULL);
-	return (bc_tick_t)( (1000000 * (now.tv_sec - start.tv_sec) )+ ( now.tv_usec - start.tv_usec ) );
+
+	return (bc_tick_t)( (1000 * (now.tv_sec - start.tv_sec) ) + (( now.tv_usec - start.tv_usec )/1000) );
 }
