cf886f3dc372b06e42063321ee452936f905c12e
Karel Blavka <karel.blavka@bigclown.com>
2016-10-25 10:25:10 +0200

    Add base64

---
+++ src/bc_base64.h
@@ -0,0 +1,12 @@
+#ifndef _BC_BASE64_H
+#define _BC_BASE64_H
+
+#include "bc_common.h"
+
+bool bc_base64_encode(char *output, size_t *output_length, uint8_t *input, size_t input_length);
+bool bc_base64_decode(uint8_t *output, size_t *output_length, char *input, size_t input_length);
+
+size_t bc_base64_calculate_encode_length(size_t length);
+size_t bc_base64_calculate_decode_length(char * input, size_t length);
+
+#endif /* _BC_BASE64_H */
