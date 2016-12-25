2dded4639b5b2215b33d453c7909b5fcf010da84
Jakub Jirutka <jakub@jirutka.cz>
2016-12-25 12:57:37 +0100

    Fix code-style and shebang in bus_status.py

--- tools/bus_status.py
+++ tools/bus_status.py
@@ -1,25 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import sys
 
 bus_status = int(sys.argv[1], 16)
 
-if (bus_status & 0x01) != 0 :
+if (bus_status & 0x01) != 0:
     print("bit 0 = controller busy: all other status bits invalid")
 
-if (bus_status & 0x02) != 0 :
+if (bus_status & 0x02) != 0:
     print("bit 1 = error condition")
 
-if (bus_status & 0x04) != 0 :
+if (bus_status & 0x04) != 0:
     print("bit 2 = slave address was not acknowledged during last")
 
-if (bus_status & 0x08) != 0 :
+if (bus_status & 0x08) != 0:
     print("bit 3 = data not acknowledged during last operation")
 
-if (bus_status & 0x10) != 0 :
+if (bus_status & 0x10) != 0:
     print("bit 4 = arbitration lost during last operation")
 
-if (bus_status & 0x20) != 0 :
+if (bus_status & 0x20) != 0:
     print("bit 5 = controller idle")
 
-if (bus_status & 0x40) != 0 :
+if (bus_status & 0x40) != 0:
     print("bit 6 = bus busy")
