42bce1fcd774cd94042f9cf408e7bf91be463464
Jakub Jirutka <jakub@jirutka.cz>
2016-12-26 02:37:30 +0100

    Set up building on Travis

---
+++ script/utils.sh
@@ -0,0 +1,33 @@
+# vim: set ts=4:
+
+die() {
+	# bold red
+	printf '\033[1;31mERROR:\033[0m %s\n' "$1" >&2
+	shift
+	printf '  %s\n' "$@"
+	exit 2
+}
+
+einfo() {
+	# bold cyan
+	printf '\n\033[1;36m> %s\033[0m\n' "$@" >&2
+}
+
+# Prints the given arguments and runs them.
+run() {
+	printf '$ %s\n' "$*"
+	"$@"
+}
+
+# Fetches the given URL and verifies SHA256 checksum.
+wgets() {(
+	local url="$1"
+	local sha256="$2"
+	local dest="${3:-.}"
+
+	mkdir -p "$dest" \
+		&& cd "$dest" \
+		&& rm -f "${url##*/}" \
+		&& wget -T 10 "$url" \
+		&& echo "$sha256  ${url##*/}" | sha256sum -c
+)}
